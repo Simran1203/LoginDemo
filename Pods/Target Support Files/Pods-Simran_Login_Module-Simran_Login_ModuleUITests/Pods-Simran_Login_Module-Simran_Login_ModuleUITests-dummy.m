e73e51306817c65a435195b2175996065e786905
simran1203 <simran.kaur@agicent.com>
2021-04-01 00:56:37 +0530

    Pods added

---
+++ Pods/Target Support Files/Pods-Simran_Login_Module-Simran_Login_ModuleUITests/Pods-Simran_Login_Module-Simran_Login_ModuleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Simran_Login_Module_Simran_Login_ModuleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Simran_Login_Module_Simran_Login_ModuleUITests
+@end
